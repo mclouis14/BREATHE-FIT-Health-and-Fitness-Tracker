a3afb284c90c8a492db446773bfc66ca09f1f3a6
LeoObinna <leoaniude20@gmail.com>
2024-09-18 10:03:16 +0100

    setup models UPDATED

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,29 +1,31 @@
+// Import the mongoose library
 import mongoose from "mongoose";
 
+// Define the schema for the "User" model
 const UserSchema = new mongoose.Schema(
     {
-        name: {
+        name: { // User's name
             type: String,
             required: true,
         },
-        email: {
+        email: { // User's email (must be unique)
             type: String,
             required: true,
             unique: true,
         },
-        password: {
+        password: { // User's password
             type: String,
             required: true,
         },
-        img: {
+        img: { // Optional user profile image URL
             type: String,
             default: null,
         },
-        age: {
+        age: { // User's age (optional)
             type: Number,
         },
     },
-    { timestamps: true }
+    { timestamps: true } // Automatically adds createdAt and updatedAt fields
 );
-
+// Export the "User" model based on the schema
 export default mongoose.model("User", UserSchema);
