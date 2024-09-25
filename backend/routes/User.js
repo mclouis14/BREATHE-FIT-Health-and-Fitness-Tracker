c87b24bc7a27acc9f6d436ce38b1366e43816416
Kingsley <louis.officialz@gmail.com>
2024-09-26 00:52:04 +0100

    Update routes

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/signup", UserRegister);
 router.post("/signin", UserLogin);
 
-router.get("/dashboard", verifyToken, getUserDashboard);
+router.post("/dashboard", verifyToken, getUserDashboard);
 router.get("/workout", verifyToken, getWorkoutsByDate);
 router.post("/workout", verifyToken, addWorkout);
 
